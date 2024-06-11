48fc42beb70fd06b8968d7f8d11048e2472d0bca
Francisco J. Casillas <chiccasillas@gmail.com>
2024-06-11 13:15:54 -0600

    Create . InputTemperature component

---
+++ src/components/InputTemperature.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function InputTermometer() {
+	const [initialTemperature, setInitialTemperature] = useState("");
+
+	const handleInitialTemperature = (event) => {
+		event.preventDefault();
+		setInitialTemperature(event.target.value);
+	};
+
+	return (
+		<div>
+			<label htmlFor="temperature">Enter initial temperature</label>
+			<input
+				id="temperature"
+				type="number"
+				value={initialTemperature}
+				onChange={(event) => handleInitialTemperature(event)}
+			/>
+		</div>
+	);
+}
+
+export default InputTermometer;
